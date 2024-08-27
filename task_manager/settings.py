bc50168526cd59f8b2d6cf1a4de881abb2f9e042
code-begemot <code.begemode@gmail.com>
2024-08-27 23:38:36 +0500

    ru

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -151,8 +151,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'ru'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 LOCALE_PATHS = [
     '/locale',
